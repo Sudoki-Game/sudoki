9ad5c13cff5489444157dcff04be97d0593b2025
Dylan Almond <dylan@dylanalmond.net>
2025-12-08 16:37:05 +0000

    minor tweak

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from '../../public/logo.svg';
 const Header = () => {
   return (
     <header className='header'>
-      <Image src={Logo} alt='Sudoki! Logo' height={84} width={275} />
+      <Image src={Logo} alt='Sudoki! Logo' loading='eager' height={84} width={275} />
     </header>
   );
 };
