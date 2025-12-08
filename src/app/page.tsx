ecce5e1d0c22f8975de5e46429fe1af988f80f79
Dylan Almond <dylan@dylanalmond.net>
2025-12-08 12:46:58 +0000

    remove theme context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import Header from '@/components/Header';
 import Sudoku from '@/components/Sudoku';
 import { SudokuProvider } from '@/context/SudokuContext';
-import { ThemeProvider } from '@/context/ThemeContext';
 
 export default function Home() {
   return (
     <main>
-      <ThemeProvider>
-        <SudokuProvider>
-          <Header />
-          <Sudoku />
-        </SudokuProvider>
-      </ThemeProvider>
+      <SudokuProvider>
+        <Header />
+        <Sudoku />
+      </SudokuProvider>
     </main>
   );
 }
