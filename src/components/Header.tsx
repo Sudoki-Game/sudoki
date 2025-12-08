2e81fdbf38933968a458a503dab4783b8abd0f61
Dylan Almond <dylan@dylanalmond.net>
2025-12-08 12:46:42 +0000

    swap outdated assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,11 @@
 'use client';
-import ThemeToggle from './ThemeToggle';
 import Image from 'next/image';
-import Github from '@/assets/github.svg';
+import Logo from '../../public/logo.svg';
 
 const Header = () => {
   return (
     <header className='header'>
-      <ThemeToggle />
-
-      <h1>Sudoki!</h1>
-
-      <a
-        className='button button--outline button--icon'
-        href='https://github.com/DylanAlmond/sudoku.git'
-        title='Github'
-      >
-        <Image src={Github} alt='GitHub Icon' />
-      </a>
+      <Image src={Logo} alt='Sudoki! Logo' height={84} width={275} />
     </header>
   );
 };
