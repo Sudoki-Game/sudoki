ad4bf41f4d0d1772a962c11dd6021678df79fe51
Dylan Almond <dylan@dylanalmond.net>
2025-12-09 18:33:26 +0000

    resposive css tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 'use client';
 import Image from 'next/image';
 import Logo from '../../public/logo.svg';
+import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
-      <Image src={Logo} alt='Sudoki! Logo' loading='eager' height={84} width={275} />
+      <Image
+        className='header__logo'
+        src={Logo}
+        alt='Sudoki! Logo'
+        loading='eager'
+        height={84}
+        width={275}
+      />
     </header>
   );
 };
