f8cde248bcb95b5902767ced284b0931ff896404
Dylan Almond <dylan@dylanalmond.net>
2025-12-11 15:02:59 +0000

    add menu buttons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 import Logo from '../../public/logo.svg';
+import GearIcon from '../../public/game/gear.svg';
+import CrownIcon from '../../public/game/crown.svg';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
+      <button className='button button--icon' type='button' onClick={() => openMenu('settings')}>
+        <Image src={GearIcon} alt='Settings Icon' height={32} width={32} />
+      </button>
+
       <Image
         className='header__logo'
         src={Logo}
@@ -13,6 +19,15 @@ const Header = () => {
         height={84}
         width={275}
       />
+
+      <button
+        disabled
+        className='button button--icon button--warning'
+        type='button'
+        onClick={() => openMenu('leaderboard')}
+      >
+        <Image src={CrownIcon} alt='Leaderboard Icon' height={32} width={32} />
+      </button>
     </header>
   );
 };
