5310c88987ba19330e278b8f0c3e0790c5abd296
Dylan Almond <dylan@dylanalmond.net>
2026-02-03 17:27:13 +0000

    add privacy policy page

---
+++ src/app/privacy/layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+import Copyright from '@/ui/components/Copyright';
+import headerStyles from '@/game/components/Header.module.css';
+
+const PrivacyLayout = async ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <header className={headerStyles.header}>
+        <span></span>
+        <Image
+          className={headerStyles.logo}
+          src={'/logo.svg'}
+          alt='Sudoki! Logo'
+          loading='eager'
+          height={84}
+          width={275}
+        />
+        <span></span>
+      </header>
+
+      <main>{children}</main>
+
+      <Copyright />
+    </>
+  );
+};
+
+export default PrivacyLayout;
