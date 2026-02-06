01535dc374f28552399fe69483aff69b67c01403
Dylan Almond <dylan@dylanalmond.net>
2026-02-06 19:09:05 +0000

    remove logs on build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
 };
 
 export default nextConfig;
