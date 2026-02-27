cf62acdf2cc9ed2bf0c4f3004f7d81bfdab2dc67
Dylan Almond <dylan@dylanalmond.net>
2026-02-27 01:37:08 +0000

    remove changelog generation

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -12,7 +12,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
@@ -26,14 +25,6 @@ module.exports = {
         assets: ['package.json', 'CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
-    // Remove CHANGELOG.md on prerelease branches to avoid merge conflict
-    [
-      '@semantic-release/exec',
-      {
-        prepareCmd:
-          "test ${branch.type} != release || sed -i '/^## \\[/h;x;/^[^]]*-/{x;d};x' CHANGELOG.md",
       },
     ],
   ],
