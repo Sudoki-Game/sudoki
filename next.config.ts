b1f166805e1332b2179fb100b35d481435273a58
Dylan Almond <dylan@dylanalmond.net>
2026-02-27 00:30:36 +0000

    ref(repo): add semantic versioning + automated build workflow

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 import type { NextConfig } from 'next';
+import git from 'git-rev-sync';
+
+function getGitHash() {
+  try {
+    return git.short();
+  } catch {
+    return process.env.GITHUB_SHA?.slice(0, 7) ?? 'dev';
+  }
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,6 +15,9 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole:
       process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
+  },
+  env: {
+    NEXT_PUBLIC_VERSION: `${process.env.npm_package_version}+${getGitHash()}`,
   },
 };
 
